179fc638aa7b15902edf64d5a96bce107143999b
BenPease21 <BAJA154@outlook.com>
2025-02-06 03:40:48 -0600

    Create The Replicator of D’To.cs

---
+++ The Replicator of D’To.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+
+namespace The_Replicator_of_D_To
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] scores = new int[5];
+
+            for (int i = 0; i < 5; i=i+1)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                scores[i] = a;
+            }
+
+            int[] scores2 = new int[5];
+
+            for (int i = 0; i < 5; i = i + 1)
+            {
+                scores2[i] = scores[i];
+            }
+
+            Console.Write("\nOriginal List: ");
+            for (int i = 0; i < 5; i=i + 1)
+            {
+                Console.Write(scores[i]);
+            }
+
+            Console.Write("\nDuplicated List: ");
+            for (int i = 0; i < 5; i = i + 1)
+            {
+                Console.Write(scores2[i]);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
