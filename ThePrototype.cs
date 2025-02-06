85aef2be2dbdca03be1d12e4065b7be6c5d03997
BenPease21 <BAJA154@outlook.com>
2025-02-06 01:34:56 -0600

    Added The Prototype

---
+++ ThePrototype.cs
@@ -0,0 +1,34 @@
+﻿// Written by Ben Pease
+
+
+using System.Runtime.InteropServices;
+
+namespace ThePrototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int AirshipNum=0;
+            int HunterNum=0;
+            int HUnterGuess;
+
+            Console.Write("User 1, enter a number between 0 and 100: ");
+            AirshipNum=Convert.ToInt32(Console.ReadLine());
+            for (int x = 0; x <= 30; x++)
+            {
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("User 2, guess the number");
+            while (true)
+            {
+             HunterNum=Convert.ToInt32(Console.ReadLine());
+                if (HunterNum > AirshipNum) { Console.WriteLine($"{HunterNum} is too high."); }
+                else if (HunterNum < AirshipNum) { Console.WriteLine($"{HunterNum} is too low."); }
+                else if (HunterNum == AirshipNum) { Console.WriteLine($"You guessed the number!"); break; }
+                Console.WriteLine("What is your next guess?");
+            }
+        }
+    }
+}
