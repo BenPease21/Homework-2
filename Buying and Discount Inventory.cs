94f0e23f297f10b1e886da4bc30f4ca9dac42a5c
BenPease21 <BAJA154@outlook.com>
2025-02-05 23:39:50 -0600

    Uploaded Buying, and Discount Inventory. Added Watchtower.

---
+++ Buying and Discount Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Intrinsics.X86;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+
+namespace Buying_Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine
+            (
+            "The following items are available:\n" +
+            "1 – Rope\n" +
+            "2 – Torches\n" +
+            "3 – Climbing Equipment\n" +
+            "4 – Clean Water\n" +
+            "5 – Machete\n" +
+            "6 – Canoe\n" +
+            "7 – Food Supplies\n" +
+            "What is your name? (Discount name is \"Ben\" btw) \n"
+             );
+            double Discount = 1;
+
+            string FirstName;
+            FirstName = Console.ReadLine();
+            if (FirstName == "Ben") { Discount = .5; }
+            else { Discount = 1; }
+
+            Console.WriteLine("What number do you want to see the price of?\n");
+
+            string Response;
+            Response = Console.ReadLine();
+
+            if (Response == "1") { Console.WriteLine(10*Discount +" Gold"); }
+            else if (Response == "2") { Console.WriteLine(16*Discount +" Gold"); }
+            else if (Response == "3") { Console.WriteLine(24 * Discount + " Gold"); }
+            else if (Response == "4") { Console.WriteLine(2 * Discount + "Gold"); }
+            else if (Response == "5") { Console.WriteLine(20 * Discount + " Gold"); }
+            else if (Response == "6") { Console.WriteLine(200 * Discount + " Gold"); }
+            else if (Response == "7") { Console.WriteLine(2 * Discount + " Gold"); }
+        }
+
+    }
+}
---
+++ Watchtower.cs
@@ -0,0 +1,80 @@
+﻿// Written by Ben Pease
+// 2/5/2025
+
+
+using System;
+
+namespace Watchtower2
+{
+    internal class WatchTower
+    {
+        static void Main(string[] args)
+        {
+            Watchtower tower = new Watchtower();
+            tower.Inputxvalue = 5;
+            tower.Inputyvalue = -3;
+            Console.WriteLine("Output X: " + tower.Outputxvalue + "\nOutput Y: " + tower.Outputyvalue);
+            Console.WriteLine("Direction: " + Watchtower.Direction(tower.Outputxvalue, tower.Outputyvalue));
+        }
+    }
+
+    public class Watchtower
+    {
+        private int inputxvalue = -1000;
+        private int inputyvalue = -1000;
+
+        public int Inputxvalue
+        {
+            get { return inputxvalue; }
+            set { inputxvalue = value; }
+        }
+
+        public int Inputyvalue
+        {
+            get { return inputyvalue; }
+            set { inputyvalue = value; }
+        }
+
+        public int Outputxvalue
+        {
+            get
+            {
+                if (Inputxvalue < 0) return -1;
+                if (Inputxvalue == 0) return 0;
+                return 1;
+            }
+        }
+
+        public int Outputyvalue
+        {
+            get
+            {
+                if (Inputyvalue < 0) return -1;
+                if (Inputyvalue == 0) return 0;
+                return 1;
+            }
+        }
+
+        public static string Direction(int xdirection, int ydirection)
+        {
+            if (xdirection < 0)
+            {
+                if (ydirection > 0) return "North West";
+                if (ydirection == 0) return "West";
+                return "South West";
+            }
+            else if (xdirection == 0)
+            {
+                if (ydirection > 0) return "North";
+                if (ydirection == 0) return "Here";
+                return "South";
+            }
+            else // xdirection > 0
+            {
+                if (ydirection > 0) return "North East";
+                if (ydirection == 0) return "East";
+                return "South East";
+            }
+        }
+    }
+}
